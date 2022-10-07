71475339d698f1fe64a710a68760f29a0450c9c4
pigobocian <pigobocian@gmail.com>
2022-10-07 14:42:31 +0200

    Dalsza praca

--- OverPutty/Log.cs
+++ OverPutty/Log.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OverPutty
 {
     class Log
     {
+        StringWriter logFile = null;
+
         public Log()
         {
-
+            string exePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            if(!exePath.EndsWith(@"\"))
+            {
+                exePath += @"\";
+            }
+            logFile = new StringWriter(exePath+"log.log");
         }
     }
 }
